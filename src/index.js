a4c5724697baca647ac97c158d42ed56a6690a3d
IceCrid <icecrid@gmail.com>
2022-08-11 18:23:32 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const numberToAbs = Math.abs(n);
+  const numberToString = numberToAbs.toString();
+  const stringToArray = numberToString.split("");
+  const reverseArray = stringToArray.reverse();
+  const arrayToString = reverseArray.join("");
+  return arrayToString;
 }
